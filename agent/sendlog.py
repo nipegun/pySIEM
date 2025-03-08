1ec7e16359f4d25d1e16b18117739ccadbcc7372
NiPeGun <nipegun@gmail.com>
2025-03-09 00:52:22 +0100

    Create sendlog.py

---
+++ agent/sendlog.py
@@ -0,0 +1,36 @@
+import sys
+import requests
+from urllib.parse import quote
+
+# Verificar que se pasaron los 4 argumentos requeridos
+if len(sys.argv) != 5:
+  print("Uso: python sendlog.py <protocolo> <ip> <puerto> <carpeta>")
+  sys.exit(1)
+
+# Obtener los argumentos de la línea de comandos
+protocolo = sys.argv[1]
+ip = sys.argv[2]
+puerto = sys.argv[3]
+carpeta = sys.argv[4]
+
+# Definir las variables
+#vProto =   sys.argv[1]
+vProto =   "http"
+#vIP =      sys.argv[2]
+vIP =      "localhost"
+#vPuerto =  sys.argv[3]
+vPuerto =  8000
+#vCarpeta = sys.argv[4]
+vCarpeta = "/log"
+
+# Popular el log
+vLogJSON = {
+  "clave1": "valor1",
+  "clave2": "valor2"
+}
+
+# Construir la URL dinámicamente
+vURL = f"{vProto}://{vIP}:{vPuerto}{vCarpeta}"
+
+# Enviar la alerta con la URL construida dinámicamente
+requests.post(vURL, json=vLogJSON)
